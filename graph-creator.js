8181f130c010480652f9642867d794bf474bab86
Colorado Reed <colorado.j.reed@gmail.com>
2013-10-05 13:34:12 -0700

    added upload/download/delete graph functionality

--- graph-creator.js
+++ graph-creator.js
@@ -1,4 +1,4 @@
-document.onload = (function(d3){
+document.onload = (function(d3, saveAs, Blob, undefined){
   "use strict";
 
   /** PREP **/
@@ -102,6 +102,16 @@ document.onload = (function(d3){
       d.y +=  d3.event.dy;
       updateGraph();
     }
+  }
+
+  function deleteGraph(skipPrompt){
+    var doDelete = true;
+    if (!skipPrompt){
+      doDelete = window.confirm("Press OK to delete this graph");
+    }
+    nodes = [];
+    edges = [];
+    updateGraph();
   }
 
   /* select all text in element: taken from http://stackoverflow.com/questions/6139107/programatically-select-text-in-a-contenteditable-html-element */
@@ -450,4 +460,52 @@ var w = window,
   }
   window.onresize = updateWindow;
 
-})(window.d3);
+  // handle download data
+  d3.select("#download-input").on("click", function(){
+    var saveEdges = [];
+    edges.forEach(function(val, i){
+      saveEdges.push({source: val.source.id, target: val.target.id});
+    });
+    var blob = new Blob([window.JSON.stringify({"nodes": nodes, "edges": saveEdges})], {type: "text/plain;charset=utf-8"});
+    saveAs(blob, "mydag.json");
+  });
+
+  // handle uploaded data
+  d3.select("#upload-input").on("click", function(){
+    document.getElementById("hidden-file-upload").click();
+    });
+  d3.select("#hidden-file-upload").on("change", function(){
+    if (window.File && window.FileReader && window.FileList && window.Blob) {
+      var uploadFile = this.files[0];
+      var filereader = new FileReader();
+            
+      filereader.onload = function(){
+        var txtRes = filereader.result;
+        // TODO better error handling
+        try{
+          var jsonObj = JSON.parse(txtRes);
+          deleteGraph(true);
+          nodes = jsonObj.nodes;
+          edges = jsonObj.edges;
+          edges.forEach(function(e, i){
+            edges[i] = {source: nodes.filter(function(n){return n.id == e.source;})[0],
+                        target: nodes.filter(function(n){return n.id == e.target;})[0]};
+          });
+          updateGraph();
+         }catch(err){
+           window.alert("Error parsing uploaded file\nerror message: " + err.message);
+           return;
+         }
+        
+      };
+      filereader.readAsText(uploadFile);
+  
+    } else {
+      alert("Your browser won't let you save this graph -- try upgrading your browser to IE 10+ or Chrome or Firefox.");
+    }
+  });
+
+  // handle delete graph
+  d3.select("#delete-graph").on("click", deleteGraph);
+
+})(window.d3, window.saveAs, window.Blob);
